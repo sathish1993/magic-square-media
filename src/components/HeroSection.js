7dc4e39588610bea10f0b11bdf982ce650eba54e
Sathish Kumar Santhanagopalan <sathish@sathishs-mbp.home>
2021-04-11 19:56:41 -0500

    magic square media init

---
+++ src/components/HeroSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+    	<img src='/images/img1.jpg' />
+    	<h1>ADVENTURE AWAITS</h1>
+    </div>
+    );
+}
+
+export default HeroSection;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+
+import HeroSection from '../HeroSection';
+
+function Home() {
+	return (
+	    <>
+	      <HeroSection />
+	    {/*  <Cards />
+      	<Footer />*/}
+	    </>
+  );
+}
+
+export default Home;
